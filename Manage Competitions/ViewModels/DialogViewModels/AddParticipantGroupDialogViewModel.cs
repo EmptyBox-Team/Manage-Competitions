fa632c71c0092e0fdb392dfa5c504be47a16d32f
Артём Гартман <chopktv@gmail.com>
2020-03-14 17:12:34 +0700

    Dialog window for make a Participant group added

---
+++ Manage Competitions/ViewModels/DialogViewModels/AddParticipantGroupDialogViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Manage_Competitions.Commands;
+using Models;
+using Models.CompetitionSystems;
+using Models.Services.DialogServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Manage_Competitions.ViewModels.DialogViewModels
+{
+    public class AddParticipantGroupDialogViewModel : ManageCompetitionsBaseViewModel, IDialogRequestClose
+    {
+        public override string Title => throw new NotImplementedException();
+        public WeightCategory WeightCategory { get; set; }
+        public CompetitionSystemType CompetitionSystemType { get; set; }
+        public ObservableCollection<Participant> Participants { get; set; }
+
+        public event EventHandler<DialogCloseRequestedEventArgs> CloseRequested;
+
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICommand OkCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public AddParticipantGroupDialogViewModel(ObservableCollection<Participant> participants)
+        {
+            Participants = participants;
+        }
+
+        public AddParticipantGroupDialogViewModel()
+        {
+            OkCommand = new RelayCommand(p => CloseRequested?.Invoke(this, new DialogCloseRequestedEventArgs(true)));
+            CancelCommand = new RelayCommand(p => CloseRequested?.Invoke(this, new DialogCloseRequestedEventArgs(false)));
+        }
+
+    }
+}

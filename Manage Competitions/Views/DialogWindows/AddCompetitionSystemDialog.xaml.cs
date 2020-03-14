9c8e0af2984da5e2ffbbf03fe3d0e7c79ecd55b1
Артём Гартман <chopktv@gmail.com>
2020-03-14 17:13:29 +0700

    Dialog window for make competition system realise

---
+++ Manage Competitions/Views/DialogWindows/AddCompetitionSystemDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Models.Services.DialogServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Manage_Competitions.Views.DialogWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCompetitionSystemDialog.xaml
+    /// </summary>
+    public partial class AddCompetitionSystemDialog : Window, IDialog
+    {
+        public AddCompetitionSystemDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}

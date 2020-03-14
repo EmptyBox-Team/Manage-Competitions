70935c904dba0bed95c7f50e490b3189515185dc
Артём Гартман <chopktv@gmail.com>
2020-03-14 17:10:50 +0700

    Navigation service realise

---
+++ Manage Competitions/State/Navigator/NavigationMenu.cs
@@ -0,0 +1,56 @@
+﻿using Manage_Competitions.Commands;
+using Manage_Competitions.Services;
+using Manage_Competitions.ViewModels;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Manage_Competitions.State.Navigator
+{
+    public enum Pages {HomePage, WeighingProtocolPage, CompetitionСoursePage, EditCompetitionPage }
+    public class NavigationMenu : NotifyModelBase,  INavigator
+    {
+        private ManageCompetitionsBaseViewModel _currentViewModel;
+        private ICommand _updateCurrentViewModel;
+        private Competition _competition;
+
+        public ManageCompetitionsBaseViewModel CurrentViewModel 
+        { 
+            get 
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NavigationMenu(Competition сompetition)
+        {
+            _competition = сompetition;
+        }
+
+        public ICommand UpdateCurrentViewModel
+        {
+            get
+            {
+                    return _updateCurrentViewModel ??
+                    (_updateCurrentViewModel = new RelayCommand(parametr =>
+                    {
+                        if (parametr is Pages)
+                        {
+                            var page = (Pages)parametr;
+                            CurrentViewModel = new WeighingProtocolBuilder(_competition).Do(page);
+                        }
+                    }));
+            }
+        }
+
+    }
+}
---
+++ Manage Competitions/State/Navigator/Navigator.cs
@@ -0,0 +1,75 @@
+﻿using Manage_Competitions.Commands;
+using Manage_Competitions.ViewModels;
+using Manage_Competitions.Views.DialogWindows;
+using Manage_Competitions.Views.MainApplicationViews.Windows;
+using Models;
+using Models.Services.DialogServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Manage_Competitions.State.Navigator
+{
+    enum ViewType { StartPage, CreateMasterPage}
+    public class Navigator : NotifyModelBase, INavigator 
+    {
+        private ICommand _updateCurrentViewModel;
+        private ICommand _initializeWorkSpaceWindow;
+        private ManageCompetitionsBaseViewModel _currentViewModel;
+        public ManageCompetitionsBaseViewModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Navigator()
+        {
+            CurrentViewModel = new StartPageViewModel(this);
+        }
+
+        public ICommand UpdateCurrentViewModel
+        {
+            get
+            {
+                 return _updateCurrentViewModel ??
+                    (_updateCurrentViewModel = new RelayCommand(parametr =>
+                    {
+                        if (parametr is ViewType)
+                        {
+                            var viewType = (ViewType)parametr;
+                            switch(viewType)
+                            {
+                                case ViewType.CreateMasterPage:
+                                    CurrentViewModel = new CreateMasterPageViewModel(this);
+                                    break;
+                                case ViewType.StartPage:
+                                    CurrentViewModel = new StartPageViewModel(this);
+                                    break;
+                            }
+                        }
+                    }));
+            }
+        }
+
+        public ICommand InitializeWorkSpaceWindow => _initializeWorkSpaceWindow ??
+                    (_initializeWorkSpaceWindow = new RelayCommand(parametr =>
+                        {
+                            var workSpaceWindow = new WorkSpaceWindow();
+                            workSpaceWindow.Show();
+                            var competition = (Competition)parametr;
+                            workSpaceWindow.DataContext = new WorkSpaceWindowViewModel(competition);
+                            Application.Current.MainWindow = workSpaceWindow;
+                        }));
+
+    }
+}
